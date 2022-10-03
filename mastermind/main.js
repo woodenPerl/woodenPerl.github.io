8ae4b19984c381bd9aaf2cfa6749aee49a916359
Moshe <105514675+woodenperl@users.noreply.github.com>
2022-10-02 20:52:30 -0400

    Add files via upload
    commit

---
+++ mastermind/main.js
@@ -0,0 +1,121 @@
+
+//set color one
+document.getElementById('pickOne') .onclick = function(){
+    document.querySelector('.hide-one') .style = 'display: flex';
+
+    //pick blue
+    document.getElementById('pickBlueOne') .onclick = function(){
+        document.getElementById('pickOne') .style = 'background-color: blue';
+    }
+
+    //pick red
+    document.getElementById('pickRedOne') .onclick = function(){
+        document.getElementById('pickOne') .style = 'background-color: red';
+    }
+
+    //pick orange
+    document.getElementById('pickOrangeOne') .onclick = function(){
+        document.getElementById('pickOne') .style = 'background-color: orange';
+    }
+
+    //pick yello
+    document.getElementById('pickYelloOne') .onclick = function(){
+        document.getElementById('pickOne') .style = 'background-color: yellow';
+    }
+
+    //close
+    document.getElementById('setPickOne') .onclick = function(){
+        document.querySelector('.hide-one') .style = 'display: none'
+        console.log('hi');
+    }
+}
+
+//set color two
+document.getElementById('pickTwo') .onclick = function(){
+    document.querySelector('.hide-two') .style = 'display: flex';
+
+    //pick blue
+    document.getElementById('pickBlueTwo') .onclick = function(){
+        document.getElementById('pickTwo') .style = 'background-color: blue';
+    }
+
+    //pick red
+    document.getElementById('pickRedTwo') .onclick = function(){
+        document.getElementById('pickTwo') .style = 'background-color: red';
+    }
+
+    //pick orange
+    document.getElementById('pickOrangeTwo') .onclick = function(){
+        document.getElementById('pickTwo') .style = 'background-color: orange';
+    }
+
+    //pick yello
+    document.getElementById('pickYelloTwo') .onclick = function(){
+        document.getElementById('pickTwo') .style = 'background-color: yellow';
+    }
+
+    //close
+    document.getElementById('setPickTwo') .onclick = function(){
+        document.querySelector('.hide-two') .style = 'display: none'
+    }
+}
+
+//set color three
+document.getElementById('pickThree') .onclick = function(){
+    document.querySelector('.hide-three') .style = 'display: flex';
+
+    //pick blue
+    document.getElementById('pickBlueThree') .onclick = function(){
+        document.getElementById('pickThree') .style = 'background-color: blue';
+    }
+
+    //pick red
+    document.getElementById('pickRedThree') .onclick = function(){
+        document.getElementById('pickThree') .style = 'background-color: red';
+    }
+
+    //pick orange
+    document.getElementById('pickOrangeThree') .onclick = function(){
+        document.getElementById('pickThree') .style = 'background-color: orange';
+    }
+
+    //pick yello
+    document.getElementById('pickYelloThree') .onclick = function(){
+        document.getElementById('pickThree') .style = 'background-color: yellow';
+    }
+
+    //close
+    document.getElementById('setPickThree') .onclick = function(){
+        document.querySelector('.hide-three') .style = 'display: none'
+    }
+}
+
+//set color four
+document.getElementById('pickFour') .onclick = function(){
+    document.querySelector('.hide-four') .style = 'display: flex';
+
+    //pick blue
+    document.getElementById('pickBlueFour') .onclick = function(){
+        document.getElementById('pickFour') .style = 'background-color: blue';
+    }
+
+    //pick red
+    document.getElementById('pickRedFour') .onclick = function(){
+        document.getElementById('pickFour') .style = 'background-color: red';
+    }
+
+    //pick orange
+    document.getElementById('pickOrangeFour') .onclick = function(){
+        document.getElementById('pickFour') .style = 'background-color: orange';
+    }
+
+    //pick yello
+    document.getElementById('pickYelloFour') .onclick = function(){
+        document.getElementById('pickFour') .style = 'background-color: yellow';
+    }
+
+    //close
+    document.getElementById('setPickFour') .onclick = function(){
+        document.querySelector('.hide-four') .style = 'display: none'
+    }
+}
